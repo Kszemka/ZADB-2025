ea6af335f8777ab593007e572c50bdce3bf7da80
Konrad <krukarkonrad@gmail.com>
2025-05-21 19:01:48 +0200

    agressive logging

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -55,6 +55,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.active_record.logger = Logger.new(STDOUT)
+
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
